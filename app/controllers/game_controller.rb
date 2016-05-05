5010c8f58c577fa0bf8cc649cfcd3962915c58ff
Petey101 <pleong101@live.com>
2016-05-05 13:23:49 -0400

    Made the champion images into links on the user show page.

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,6 @@
+class GameController < ApplicationController
+
+  def show
+  	@game = Game.new
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427210746) do
+ActiveRecord::Schema.define(version: 20160505171846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "battles", force: :cascade do |t|
-    t.boolean  "win",         default: false
-    t.integer  "user_id",                     null: false
-    t.integer  "question_id",                 null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
-
-  add_index "battles", ["question_id"], name: "index_battles_on_question_id", using: :btree
-  add_index "battles", ["user_id"], name: "index_battles_on_user_id", using: :btree
-
-  create_table "questions", force: :cascade do |t|
     t.text     "question_text",    null: false
     t.text     "possible_answers", null: false
     t.text     "correct_answer",   null: false
@@ -35,6 +24,17 @@ ActiveRecord::Schema.define(version: 20160427210746) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  create_table "games", force: :cascade do |t|
+    t.boolean  "win",         default: false
+    t.integer  "user_id",                     null: false
+    t.integer  "question_id",                 null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "games", ["question_id"], name: "index_games_on_question_id", using: :btree
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
