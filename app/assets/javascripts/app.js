73973e747246be5cbb346a42a2935f7422d9445c
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-05-17 22:09:35 -0400

    Get Angular  Working across app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var myApp = angular.module("summoners-universe", ['ngRoute'])
-
-myApp.config(['$routeProvider', function($routeProvider){
-  $routeProvider.when("games/:gameId", {
-    templateURL: "app/views/games/show.html",
-    controller: 'CountdownController'
+  
+myApp.config(['$routeProvider', "$locationProvider", function($routeProvider, $locationProvider){
+  $routeProvider.when("/games/:gameId", {
+    controller: 'CountdownController',
+    templateUrl: "/timer.html"
+  }).otherwise({
+    redirectTo: "/"
   })
+  $locationProvider.html5Mode({ enabled: true, requireBase: false }); 
 }])
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,11 +1,4 @@
 'use strict';
-var myApp = angular.module("summoners-universe")
-
-myApp.controller('CountdownController', ["$scope", "$http", "$routeParams", "$timeout", function($scope, $http, $routeParams, $timeout){
-    $scope.$on("$routeChangeSuccess", function(){
-      $scope.endTime = $http.get("/games" + $routeParams.id).success(function(data){
-          console.log(data)
-          $scope.data = data;
-    })
-    })
-  }])
+myApp.controller('CountdownController', ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams){
+  console.log($routeParams)
+}])
