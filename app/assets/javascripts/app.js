d22bab9148b41aa0f5da973f9eb3b525e2b917d8
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-05-18 11:22:36 -0400

    Comment out config method in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,11 +2,11 @@
 
 var myApp = angular.module('summoners-universe', ['ngRoute', 'templates'])
   
-myApp.config(['$routeProvider', "$locationProvider", function($routeProvider, $locationProvider){
-  $routeProvider.when("/games/:id", {
-    controller: 'CountdownController',
-    templateUrl: "timer.html"
-  }).otherwise({
-    redirectTo: "/"
-  })
-}])
+// myApp.config(['$routeProvider', "$locationProvider", function($routeProvider, $locationProvider){
+//   $routeProvider.when("/games/:id", {
+//     controller: 'CountdownController',
+//     templateUrl: "timer.html"
+//   }).otherwise({
+//     redirectTo: "/"
+//   })
+// }])
