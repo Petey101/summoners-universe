e98637b650a19ca7205d8fca1103c6f47f3263d0
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-05-23 11:11:04 -0400

    Create working timer displaying remaining time and time's up message

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var myApp = angular.module('summoners-universe', ['ngRoute', 'templates', 'angularMoment'])
+var myApp = angular.module('summoners-universe', ['ngRoute', 'templates'])
   
 myApp.config(['$routeProvider', "$locationProvider", function($routeProvider, $locationProvider){
   $routeProvider
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -7,6 +7,11 @@ myApp.controller('CountdownController',
     Hydration.onReady(function(data){
       $scope.time_limit = data.gameMeta.time_limit
       $scope.start_time = data.gameMeta.start_time
+      var end = moment.duration($scope.time_limit, "m")._milliseconds
+      var start = moment.duration($scope.start_time, "m")._milliseconds
+      var remainingTime = moment.duration(end - start)
+      var countdown = remainingTime.minutes() + " minutes " + "and " + remainingTime.seconds() + " seconds";
+      $scope.countdown = countdown
     })
   })
 }]);
