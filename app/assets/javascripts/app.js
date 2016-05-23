5aebc7a3c94fc4809cd5339f05153eeac950bee0
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-05-23 12:03:31 -0400

    Add comment for future dev

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,5 +10,6 @@ myApp.config(['$routeProvider', "$locationProvider", function($routeProvider, $l
     controller: 'CountdownController',
     templateUrl: "timer.html"
   })
+  //need to set it so that we can go to root and user show.
   $locationProvider.html5Mode({ enabled: true, requireBase: false }); 
 }])
