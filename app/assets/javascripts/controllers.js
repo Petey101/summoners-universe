423200f49bbbb3a12a69c73e3f491f9f8f7fd7e3
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-05-25 15:16:07 -0400

    Re-save controllers file in javascripts directory

---
+++ app/assets/javascripts/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+myApp.controller('CountdownController', 
+  ["$scope", "$http", "$routeParams", "$document", "$timeout", "startTimer",
+  function($scope, $http, $routeParams, $document, $timeout,startTimer){
+  $http.get('/games/' + $routeParams.id).then(function(response){
+    Hydration.onReady(function(data){
+      $scope.time_limit = data.gameMeta.time_limit
+      $scope.start_time = data.gameMeta.start_time
+      $scope.countdown = startTimer.countdownTimer($scope.time_limit, $scope.start_time);
+      $scope.onTimeout = function(){
+        $scope.countdown--;
+        if($scope.countdown >= 0){
+          mytimeout = $timeout($scope.onTimeout, 1000);
+        }else{
+          return;
+        }
+      }
+        var mytimeout = $timeout($scope.onTimeout,1000);  
+      })
+  })
+}]);
