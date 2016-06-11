44a0043c33f5841fbe34238e86891333876fe393
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-06-11 13:21:11 -0400

    Remove refactored code.

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -10,17 +10,4 @@ myApp.service("startTimer", ["$timeout", function($timeout){
     var timer = moment.duration(remainingTime);
     return timer
 }
-
-  // this.timeOut = function(countdown, timer, url, params){
-  //       countdown = countdown - 1000
-  //       if(countdown >= 0){
-  //         var seconds = Math.floor((countdown % 60000)/1000).toString()
-  //         timer = (Math.floor(countdown/60000) + ":" + (seconds.length < 2 ? '0' + seconds : seconds))
-  //         mytimeout = $timeout(this.timeOut, 1000);
-  //       }else{
-  //         $(".countdown-timer").html("Time's Up!")
-  //         url = '/games/failed_game?id=' + params
-  //       }
-  //     }
-  //       var mytimeout = $timeout(this.timeOut,1000);
 }]);
