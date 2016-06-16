6bb7fe06498915b7abc468bcd065687358637328
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-06-16 14:33:08 -0400

    Add potential fix for bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :games
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   def self.get_skin_image(champ)
     url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=skins&api_key=#{LOL_KEY}"
     uri = URI(url)
