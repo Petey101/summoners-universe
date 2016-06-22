18f1a94a47089a6e6d491398cd682e2296b33591
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-06-22 09:32:53 -0400

    Refactor Angular controller code for clarity

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,13 +1,21 @@
 'use strict';
 
 myApp.service("startTimer", ["$timeout", function($timeout){
-  this.countdownTimer = function(endTime, startTime){
+  this.getTimer = function(endTime, startTime){
     var end = moment.duration(endTime, "m")._milliseconds;
     var start = moment.duration(startTime, "m")._milliseconds;
-    if(start > end){
-    }
     var remainingTime = moment.duration(end - start);
     var timer = moment.duration(remainingTime);
-    return timer
-}
+    return timer;
+};
+
+  this.countdownTimer = function(scope, countdown){
+    var seconds = Math.floor((countdown % 60000)/1000).toString();
+    scope.timer = (Math.floor(countdown/60000) + ":" + (seconds.length < 2 ? '0' + seconds : seconds));
+  };
+
+  this.timeUp = function(doc, routeParams){
+    $(".countdown-timer").html("Time's Up!");
+    doc.location.href = '/games/failed_game?id=' + routeParams.id;
+  };
 }]);
