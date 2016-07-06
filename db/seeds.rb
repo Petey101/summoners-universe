63beafee9c43eb3ab405e1c9dd43d3b37f363bfd
Petey101 <pleong101@live.com>
2016-07-06 17:57:16 -0400

    Create more diverse questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,23 @@ Battle.create(question_text: "The enemy team is building large amounts of magic 
 Battle.create(question_text: "The enemy team has many champions with crowd control spells. Which item would give you more time to control your character?" ,possible_answers: [[1,3111],[2,3078],[3,3098],[4,3100]], correct_answer: 1)
 # Battle.create(question_text: "You've learned from the internet that it is a bad idea to face check bushes. Which trinket would be best for checking if anyone is hiding in a bush from far away?" ,possible_answers: [[1,3363],[2,3262],[3,3361],[4,3364]], correct_answer: 1)
 Battle.create(question_text: "Which pair of boots would allow you to cast your summoner spells more often?" ,possible_answers: [[1,3158],[2,3006],[3,3009],[4,3047]], correct_answer: 1)
+
+Battle.create(question_text: "Which support item would allow you to shield your allies based on your own health?" ,possible_answers: [[1,3401],[2,3092],[3,3069],[4,3070]], correct_answer: 1)
+Battle.create(question_text: "Which support item would allow you to slow down your enemies so your team can catch them?" ,possible_answers: [[1,3401],[2,3092],[3,3069],[4,3070]], correct_answer: 2)
+Battle.create(question_text: "Which support item would speed up your team's movement speed to either catch or escape from an enemy team?" ,possible_answers: [[1,3401],[2,3092],[3,3069],[4,3070]], correct_answer: 3)
+Battle.create(question_text: "Which item would allow you to shoot extra bolts on your auto attacks as a ranged champion?" ,possible_answers: [[1,3401],[2,3092],[3,3069],[4,3085]], correct_answer: 4)
+Battle.create(question_text: "Which item would allow you to slow down enemy units you hit with your spells?" ,possible_answers: [[1,3085],[2,3116],[3,3069],[4,3085]], correct_answer: 2)
+Battle.create(question_text: "Which item would allow to turn invulnerable, but unable to act, avoiding damage and allowing abilities to come off cooldown?" ,possible_answers: [[1,3085],[2,3116],[3,3157],[4,3085]], correct_answer: 3)
+Battle.create(question_text: "Which item would allow to remove all crowd control effects on your character?" ,possible_answers: [[1,3085],[2,3116],[3,3140],[4,3085]], correct_answer: 3)
+
+
+
+
+3401 face of the mountain
+3092 frost queens claim
+3069 talisman of ascension
+3070 tear of the goddess
+3085 runaan's hurricane
+3116 rylai's crystal scepter
+3157 zhonyas hourglass
+3140 quicksilver sash
